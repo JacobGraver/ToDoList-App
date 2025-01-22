f53abf84d25b24ff71478312379ac85eeaa8ce42
Jacob Graver <jacob.graver@gmail.com>
2025-01-22 14:35:07 -0700

    1/22 edits

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+	"github.com/JacobGraver/ToDoList-App.git/todo"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("TODO App")
+
+	w.SetContent(widget.NewLabel("TODOs will go here"))
+
+	t := todo.NewTodo("Show this on the window")
+	w.SetContent(widget.NewLabel(t.String()))
+
+	w.ShowAndRun()
+}
