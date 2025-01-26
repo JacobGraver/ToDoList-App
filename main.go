109b164ec3232b24de538214c39ae228907fb1a5
Jacob Graver <jacob.graver@gmail.com>
2025-01-25 19:11:41 -0700

    1/25

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 	"github.com/JacobGraver/ToDoList-App.git/todo"
 )
@@ -10,10 +11,15 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("TODO App")
 
-	w.SetContent(widget.NewLabel("TODOs will go here"))
+	//	w.SetContent(widget.NewLabel("TODOs will go here"))
 
 	t := todo.NewTodo("Show this on the window")
-	w.SetContent(widget.NewLabel(t.String()))
+
+	w.SetContent(
+		container.NewCenter(
+			widget.NewLabel(t.String()),
+		),
+	)
 
 	w.ShowAndRun()
 }
