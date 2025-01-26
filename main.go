a3bfee43b482b569d80c6d2d2868958cc9589825
Jacob Graver <jacob.graver@gmail.com>
2025-01-25 19:13:13 -0700

    add button

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -16,10 +18,20 @@ func main() {
 	t := todo.NewTodo("Show this on the window")
 
 	w.SetContent(
-		container.NewCenter(
-			widget.NewLabel(t.String()),
+		container.NewBorder(
+			nil, // TOP of the container
+
+			// this will be a the BOTTOM of the container
+			widget.NewButton("Add", func() { fmt.Println("Add was clicked!") }),
+
+			nil, // Right
+			nil, // Left
+
+			// the rest will take all the rest of the space
+			container.NewCenter(
+				widget.NewLabel(t.String()),
+			),
 		),
 	)
-
 	w.ShowAndRun()
 }
