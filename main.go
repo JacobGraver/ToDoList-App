9a4f7e3e19ec6bee53ddffef3e1919768429b6ce
Jacob Graver <jacob.graver@gmail.com>
2025-01-26 18:04:05 -0700

    1/26

--- main.go
+++ main.go
@@ -3,18 +3,23 @@ package main
 import (
 	"fmt"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"github.com/JacobGraver/ToDoList-App.git/todo"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("TODO App")
+	w.Resize(fyne.NewSize(300, 400))
 
-	//	w.SetContent(widget.NewLabel("TODOs will go here"))
-
-	//	t := todo.NewTodo("Show this on the window")
+	data := []todo.Todo{
+		todo.NewTodo("Some stuff"),
+		todo.NewTodo("Some more stuff"),
+		todo.NewTodo("Some other things"),
+	}
 
 	newtodoDescTxt := widget.NewEntry()
 	newtodoDescTxt.PlaceHolder = "New Todo Description..."
@@ -32,12 +37,20 @@ func main() {
 	w.SetContent(
 		container.NewBorder(
 			nil, // TOP of the container
-
 			container.NewBorder(
 				nil, // TOP
 				nil, // BOTTOM
-				nil, // Left
-				// RIGHT ↓
+				widget.NewList(
+					func() int {
+						return len(data)
+					},
+					func() fyne.CanvasObject {
+						return widget.NewLabel("template")
+					},
+					func(i widget.ListItemID, o fyne.CanvasObject) {
+						o.(*widget.Label).SetText(data[i].Description)
+					},
+				),
 				addBtn,
 				// take the rest of the space
 				newtodoDescTxt,
